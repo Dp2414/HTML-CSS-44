201ffa3a6b9cb08f57b4d222f25b17d465434e93
Dp2414 <dpdp8311@gmail.com>
2025-02-25 10:06:43 +0530

    js

---
+++ JavaScript/25-02-25/script.js
@@ -0,0 +1,58 @@
+// console.log(4+5);
+// console.log(5-3);
+// console.log(5*3);
+// console.log(10%2);
+// console.log(10/5);
+// console.log(3**2);
+
+
+//Assignment Operator
+/*let x=10;
+
+x!=10;
+x+=10;
+x=x+10;
+x-=10;
+x=x-10;
+x*=10;
+x=x*10;
+console.log(x); */
+
+
+//Comparision Operator
+
+console.log(10 =="10");  //true
+console.log(10 === "10"); //false
+console.log(10 != "10");  //false   
+console.log(10 !== "10");  //true
+
+
+//Logcial Operators : && (AND),OR (||),!
+
+
+console.log(true&&false); //false
+console.log(true||false); //true
+let a =10;
+console.log(!a);
+
+
+//Ternary Operator is alsoused in REACT
+//condition ? true : false
+
+let b=18;
+let result=b>20?"you can drive":"you cannot drive";
+console.log(result);
+
+
+//typeof and instanceof
+
+console.log(typeof "10");
+
+console.log(typeof 5);
+console.log(typeof [1,2,3]);
+
+
+//String Operator
+let first="something";
+let last=" nothing";
+console.log(first+last);
