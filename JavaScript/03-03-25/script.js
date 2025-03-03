c60ba53e301b740c030260db68c82672a7ab40a9
Dp2414 <dpdp8311@gmail.com>
2025-03-03 17:16:47 +0530

    loops

---
+++ JavaScript/03-03-25/script.js
@@ -0,0 +1,104 @@
+// for(let i=10;i>=1;i--)
+// {
+   
+
+//     if(i===4)
+//         console.log(i);
+//         break;
+    
+// }
+
+//break and continue
+ // for (Let index = 10; index > 0; index--) {
+//  // if (index === 3) {
+
+    //console.log(index);
+ //break;
+     // }
+     // }
+//      for (let i = 1; i <=10; i++) {
+//       if(i==8){
+//         break;
+//       }
+//             console.log(i);
+// }
+
+
+
+// let fruits = ["apple","pineapple", "watermelon","orange","mango","grapes","banana","jack fruit",];
+    
+// for (let i = 0; i < fruits.length-8; i++) {
+//     console.log(fruits[i]);
+// }
+
+// for(let i=10;i>=1;i--)
+    // {
+       
+    
+    //     if(i===4)
+    //         console.log(i);
+    //         break;
+        
+    // }
+    // let i=1;
+    // while(i<=10){
+        
+    //     console.log(i);
+       
+    //     i++;
+    // }
+// let i=10;
+// do{
+//     i--;
+//     console.log(i);
+  
+    
+// }while(i>0);
+
+
+// let withDraw=6000;
+// let balance =5000;
+// while(withDraw>balance){
+//     console.log("entered invalid");
+//     withDraw=3000;
+   
+    
+// }
+// balance-=withDraw;
+// console.log(`${balance}`);
+
+// let i=10;
+// do{
+   
+//     console.log(i);
+//     i--;
+  
+    
+// }while(i>0);
+
+
+// let student={
+//     name:"dp",
+//     age:24,
+//     isStudent:true,
+//     id:14,
+// };
+
+// for(const key in student){
+//     console.log(`${key}: ${student[key]}`);
+    
+// }
+
+
+
+let fruits = ["apple","pineapple", "watermelon","orange","mango","grapes","banana","jack fruit",];
+let i=0;
+for( let element of fruits)
+{
+    console.log(`${i} : ${element}`);
+  
+    i++;
+    
+    
+}
+// console.log(`the length of the array is ${fruits.length}`);
